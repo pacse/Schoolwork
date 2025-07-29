c415cdae1e8334956d9d2aeb19eb8af31cfe384a
pacse <68709871+pacse@users.noreply.github.com>
2025-07-29 13:25:32 +1200

    Finished!!
    Hope this works :))

--- Year10/OpenCV/06-electric-porygon-proj.py
+++ Year10/OpenCV/06-electric-porygon-proj.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 from pathlib import Path
+from sys import exit
 
 # === paths ===
 
@@ -20,67 +21,20 @@ def init_img(size: tuple):
     return np.zeros(size, dtype = np.uint8) # ensure the zeros is one byte long
 
 
-def filter_colours(src: cv2.typing.MatLike, **kwargs) -> cv2.typing.MatLike:
+def filter_colours(src: cv2.typing.MatLike, channels: list[int]) -> cv2.typing.MatLike:
     '''
     Creates an image with the colour channels specified\n
     src: the image to filter\n
-    kwargs:\n
-    colour: The colour to make the filtered image (lowercase)\n
     channels: a list of the colour channels to include (eg [1,0,0] for blue)
     '''
 
-    _VALID_COLOURS = ["grey", "red", "green", "blue", "purple", "yellow"]
-
     # init blank img of src shape
     img = init_img(src.shape)
-
-    # handle word filters
-    if "colour" in kwargs:
-        if kwargs["colour"] == "grey":
-            img = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-
-        elif kwargs["colour"] == "red":
-            # put red (2) channel in img
-            img[:,:,2] = src[:,:,2]
-
-        elif kwargs["colour"] == "green":
-            # green channel is 1
-            img[:,:,1] = src[:,:,1]
         
-        elif kwargs["colour"] == "blue":
-            # channel is 0
-            img[:,:,0] = src[:,:,0]
-        
-        elif kwargs["colour"] == "purple":
-            # channels 0,2
-            img[:,:,0] = src[:,:,0]
-            img[:,:,2] = src[:,:,2]
-        
-        elif kwargs["colour"] == "yellow":
-            # channels 0,1
-            img[:,:,0] = src[:,:,0]
-            img[:,:,1] = src[:,:,1]
-
-        else:
-            raise ValueError(f"Invalid colour: {kwargs['colour']}\nValid colours: {_VALID_COLOURS}")
-    
-    # handle list filters 
-    elif "channels" in kwargs:
-        # check kwarg is valid
-                # channels is a list
-        if not (isinstance(kwargs["channels"], list) or
-                # all values are ints
-                any(isinstance(channel, int) for channel in kwargs["channels"]) or 
-                # only 3 channels
-                len(kwargs["channels"]) != 3):
-            
-            # give ValueError if problem
-            raise ValueError("Expected channels kwarg to be type list[int] of length 3")
-        
-        # add filtered channels to img
-        for i, channel in enumerate(kwargs["channels"]):
-            if channel == 1:
-                img[:,:,i] = src[:,:,i]
+    # add filtered channels to img
+    for i, channel in enumerate(channels):
+        if channel == 1:
+            img[:,:,i] = src[:,:,i]
 
     # return img
     return img
@@ -91,15 +45,53 @@ def filter_colours(src: cv2.typing.MatLike, **kwargs) -> cv2.typing.MatLike:
 # load src
 src = cv2.imread(str(src_path))
 
-# get filtered imgs
-filtered_imgs = [] # put imgs bere
+# ask usr if they have photosensitive epilepsy
+epilepsy = input("Do you have photosensitive epilepsy?\n(y/n) >>> ").lower()
 
-for i in range(3):
-    # channels to pass to filter_colours
-    channels = [0,0,0]
-    # set the index to filter
-    channels[i] = 1
+# if they do, show src
+if epilepsy == "y":
+    cv2.imshow("Porygon", src)
 
-    # filter the img by only important colour channels
-    # and append to filtered imgs
-    filtered_imgs.append(filter_colours(src, channels=channels))
+# if they don't, load and show flashing imgs
+elif epilepsy == "n":
+
+	# get filtered imgs
+	f_imgs = [] # put imgs bere
+
+	for i in range(3):
+	    # channels to pass to filter_colours
+    	channels = [0,0,0]
+    	# set the index to filter
+    	channels[i] = 1
+
+    	# filter only wanted colour channels from src
+    	# and append to filtered imgs
+    	f_imgs.append(filter_colours(src, channels))
+    
+    # ask usr what speed they want to flash at
+	speed = input("Do you want to have fast (0.1s) or slow (0.5s) flashing?\n(f/s) >>> ")
+    
+    # set delay time
+    if speed == "f":
+        delay = 100
+    elif speed == "s":
+        delay = 500
+    else:
+        # cry
+        print("Just leave me alone :(")
+        exit(1)
+        
+    # show all colours 10 times
+    for _ in range(10):
+        # show each img we've filtered
+        for f_img in f_imgs:
+            # show img in same window
+            cv2.imshow("Filtered Porygon", f_img)
+            # delay between imgs
+            cv2.waitKey(delay)
+                
+else:
+    # more crying
+    print("Come on, I couldn't have made it easier for you :(")
+    
+cv2.destroyAllWindows()
