0d3ffae24d76501ce0f73a28d993298b77f6bf75
pacse <68709871+pacse@users.noreply.github.com>
2025-08-01 09:43:52 +1200

    Create 07-drawing.py
    Learning to draw with list slicing! tis hard, but fun :))

---
+++ Year10/OpenCV/07-drawing.py
@@ -0,0 +1,29 @@
+import cv2 as cv5
+import numpy as np
+
+# let's draw some shapes!
+img = np.zeros((500, 500), dtype=np.uint8)
+
+# show canvas
+cv5.imshow("display", img)
+
+cv5.waitKey(0)
+
+# draw a square & show
+img[100:125, 100:125] = 255
+cv5.imshow("display", img)
+
+cv5.waitKey(0)
+
+# draw another square & show
+img[100:125, 200:225] = 255
+cv5.imshow("display", img)
+
+cv5.waitKey(0)
+
+# draw line & show
+img[150:175, 100:225] = 255
+cv5.imshow("display", img)
+
+cv5.waitKey(0)
+
